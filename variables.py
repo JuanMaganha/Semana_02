5e8122b0093c18b929de13e5dabb668fe23f78eb
JuanMaganha <20265241@esen.edu.sv>
2026-09-21 15:58:49 -0600

    Trabajando con variables

--- variables.py
+++ variables.py
@@ -1,4 +1,9 @@
 #Esta sesión sera para aprender el uso de variables
-print("Hola mundo")
-print("Hoy es miercoles")
-print("mañana es jueves")
+print("Sistema de mensajeria")
+distancia_km = 40.5
+peso_kg = 2.0
+es_urgente = True
+zona = "B"
+distancia_km = distancia_km + 16
+print(distancia_km)
+
